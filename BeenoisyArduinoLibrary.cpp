576d585d714426c03a55362adf3d056773b8e974
jacks808 <jacks808@163.com>
2014-10-31 12:20:19 +0800

    add import and library public head file
    Signed-off-by: jacks808 <jacks808@163.com>

---
+++ BeenoisyArduinoLibrary.cpp
@@ -0,0 +1,12 @@
+// Do not remove the include below
+#include "BeenoisyArduinoLibrary.h"
+
+//The setup function is called once at startup of the sketch
+void setup() {
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop() {
+//Add your repeated code here
+}
---
+++ BeenoisyArduinoLibrary.h
@@ -0,0 +1,23 @@
+/**
+ * - function definitions (prototypes)
+ * - include files
+ * - extern variable definitions
+ */
+#define _BeenoisyArduinoLibrary_H_
+#include "Arduino.h"
+//Includes for the project BeenoisyArduinoLibrary here
+
+#include "BasicFunctions.h"
+
+//end of includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//Function definitions for the project BeenoisyArduinoLibrary here
+
